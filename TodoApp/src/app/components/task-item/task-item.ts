93b4d3e0c9fb42182d359b362ee0a00d93701300
ManuPGarcia <mpgarcia1@stefanini.com>
2026-07-07 13:36:08 -0300

    subindo alteração

--- TodoApp/src/app/components/task-item/task-item.ts
+++ TodoApp/src/app/components/task-item/task-item.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import  Mock from '../../../../public/mock.json';
 
+
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.html',
